c8346831b1509d1e381a3a14fec9803503e85945
Gus Nordhielm <gnordhielm@gmail.com>
2018-02-08 11:49:38 -0800

    move config into environment variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase'
 
 const config = {
-  apiKey: "AIzaSyAxZb3SZcNqeqeIlbeJk68jLZQ7B4LzBqE",
-  authDomain: "expensify-gn.firebaseapp.com",
-  databaseURL: "https://expensify-gn.firebaseio.com",
-  projectId: "expensify-gn",
-  storageBucket: "expensify-gn.appspot.com",
-  messagingSenderId: "58882340520"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DB_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 }
 
 firebase.initializeApp(config)
