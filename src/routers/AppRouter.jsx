300b8f2bcf3a031f5514a0f9fc10d800df34f8ca
Gus Nordhielm <gnordhielm@gmail.com>
2018-02-09 20:30:12 -0800

    create public routes

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -6,9 +6,9 @@ import LoginPage from '../components/LoginPage.jsx'
 import DashboardPage from '../components/DashboardPage.jsx'
 import CreatePage from '../components/CreatePage.jsx'
 import EditPage from '../components/EditPage.jsx'
-import HelpPage from '../components/HelpPage.jsx'
 import NotFoundPage from '../components/NotFoundPage.jsx'
 import PrivateRoute from './PrivateRoute.jsx'
+import PublicRoute from './PublicRoute.jsx'
 
 export const history = createHistory()
 
@@ -16,11 +16,10 @@ const AppRouter = () => (
   <Router history={history}>
     <div>
       <Switch>
-        <Route path="/" exact component={LoginPage} />
+        <PublicRoute path="/" exact component={LoginPage} />
         <PrivateRoute path="/dashboard" component={DashboardPage} />
         <PrivateRoute path="/create" component={CreatePage} />
         <PrivateRoute path="/edit/:id" component={EditPage} />
-        <Route path="/help" component={HelpPage} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
